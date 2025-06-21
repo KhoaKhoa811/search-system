3a23e3c07fada6fa8c0e93b5bde0a1206544fb2b
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2025-06-21 23:15:12 +0700

    init project

---
+++ src/main/java/com/example/searchsystem/SearchsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/searchsystem/SearchsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
